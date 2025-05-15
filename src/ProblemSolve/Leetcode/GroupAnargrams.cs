2ad981703aaf6bd030a81319475e94e7abe47911
IrfanMahmud <irfanmahmud201@gmail.com>
2025-05-15 19:28:04 +0600

    Solved GroupAnagrams

---
+++ src/ProblemSolve/Leetcode/GroupAnargrams.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class GroupAnargrams
+    {
+        private static string Hashing(string str)
+        {
+            var arr = new int[27];
+            foreach (var ch in str)
+            {
+                arr[(ch - 'a') + 1]++;
+            }
+            return string.Join(",", arr);
+        }
+        public static IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            var ans = new List<IList<string>>();
+            var map = new Dictionary<string, int>();
+            var index = map.Count;
+            foreach (var str in strs)
+            {
+                var hash = Hashing(str);
+                if (!map.ContainsKey(hash))
+                {
+                    map[hash] = index++;
+                    ans.Add(new List<string>());
+                }
+                ans[map[hash]].Add(str);
+            }
+            return ans;
+        }
+    }
+}
--- src/ProblemSolve/Leetcode/Program.cs
+++ src/ProblemSolve/Leetcode/Program.cs
@@ -1,15 +1,10 @@
 ﻿using CodeExamples;
+using Leetcode;
 
-var s = "anagram";
-var t = "nagaram";
-var result = ValidAnargram.IsAnagram(s, t);
-//Console.WriteLine(result); 
+var strs = new string[] { "eat", "tea", "tan", "ate", "nat", "bat" };
+var result = GroupAnargrams.GroupAnagrams(strs);
 
-var nums = new int[]{ 2, 7, 11, 15 };
-var target = 9;
-var result2 = TwoSumProblem.TwoSum(nums, target);
-//Console.WriteLine($"[{result2[0]}, {result2[1]}]"); 
-
-var nums2 = new int[] { 1,2,4,7,9};
-var result3 = TwoSum_II.TwoSum( nums2, target);
-Console.WriteLine($"[{result3[0]}, {result3[1]}]");
+foreach (var group in result)
+{
+    Console.WriteLine(string.Join(", ", group));
+}
